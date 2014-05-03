cb3699b03027488ef047915f2babac5dc4d91a52
happy <yangbai6644@163.com>
2014-05-03 12:46:05 +0800

    项目底层函数库：增加错误输出函数

--- Happy/Common/functions.php
+++ Happy/Common/functions.php
@@ -18,3 +18,12 @@
 function p($arr) {
     echo '<pre>', \print_r($arr, true), '</pre>';
 }
+
+/**
+ * 友好错误信息输出
+ */
+function error($msg) {
+    $msg = '<div style="width:500px;height:100px;border:solid 1px #333;background:#f2f2f2;color:red;padding:10px;position:absolute;top:20%;">'
+            . $msg . '</div>';
+    exit($msg);
+}
