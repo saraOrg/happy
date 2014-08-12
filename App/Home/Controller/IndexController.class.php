56ba7deaaabc471bd838a493f2886f55282dc665
yangbai <yangbai@192.168.1.105>
2014-08-12 22:02:53 +0800

    Happy框架本地提交

---
+++ App/Home/Controller/IndexController.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * =================================================
+ * 本类由系统自动生成，仅供测试用途
+ * ================================================
+ * @category happy
+ * @package Admin/
+ * @subpackage Action
+ * @author Happy <yangbai6644@163.com>
+ * @dateTime 2014-5-3 16:42:41
+ * ================================================
+ */
+class IndexController extends Controller {
+
+    public function index() {
+        echo '<h1>欢迎使用HappyPHP！</h1>';
+    }
+
+}
